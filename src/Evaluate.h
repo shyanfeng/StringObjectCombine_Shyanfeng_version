4c75561d2d3c6b4f78bbe02807134afd22bb0837
James Ho <James_6318@hotmail.com>
2014-07-18 14:19:01 +0800

    done

---
+++ src/Evaluate.h
@@ -0,0 +1,6 @@
+#ifndef Evaluate_H
+#define Evaluate_H
+
+int evaluate(char *charbla); //jason shuntingyard
+
+#endif // Evaluate_H
---
+++ src/StringObject.h
@@ -0,0 +1,41 @@
+#ifndef __STRING_OBJECT_H__
+#define __STRING_OBJECT_H__
+
+#include "Types.h"
+#include "Text.h"
+
+#define isSpace(ch) (ch==' ' || ch=='\t')
+
+typedef struct String{
+	uint32 reference;
+	Text *text;
+	uint32 start;
+	uint32 length;
+}String;
+
+//test prototype
+int stringToInteger(String *str);
+char *stringSubstringInChar(String *str, int relativePosition, int length);
+Text *stringSubstringInText(String *str, int relativePosition, int length);
+
+//add stringClone, textClone
+String *stringNew(Text *text);
+String *stringAssign(String *string);
+String *stringDel(String *string);
+
+void stringSkip(String *string, int numChar2Skip);
+void stringTrimLeft(String *string);
+void stringTrimRight(String *string);
+void stringTrim(String *string);
+
+int stringRemoveChar(String *str); // return -l if empty remove by moving index number
+int stringLength(String *str); // length of string
+String *stringRemoveWordNotContaining(String *str,char delimites[]); 
+String *stringRemoveWordContaining(String *str, char containSet[]);
+int stringIsEqual(String *str1, String *str2); //1 if same, 0 otherwise do no use string compare
+int stringIsEqualCaseInsensitive(String *str1, String *str2); //uses char tolower();
+
+int stringCharAt(String *str, int relativeIndex);
+int stringIsCharAtInSet(String *str, int relativeIndex, char set[]); //1 is true, 0 false
+
+#endif // __STRING_OBJECT_H__
